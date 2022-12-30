510e7485e4cb7a9d835f9bbeff4eac62ee931941
pratapshingane18 <sushrutrao465@gmail.com>
2022-12-30 18:20:02 +0530

    Console and Variables

---
+++ app.js
@@ -0,0 +1,30 @@
+// Log to Console
+
+console.log('hello');
+console.log(123);
+console.log(true);
+
+var greeting = 'hello';
+console.log(greeting);
+
+console.log([1,2,3,4]);
+
+console.log({a:1,b:2,c:3});
+
+console.table({a:1,b:2,c:3});
+
+console.error('this might be an error');
+console.clear();
+
+console.warn("warning");
+
+console.time('Time');
+console.log('helloPrats');
+console.log('helloPrats');
+console.log('helloPrats');
+console.log('helloPrats');
+console.log('helloPrats');
+console.log('helloPrats');
+console.log('helloPrats');
+
+console.timeEnd('Time');
---
+++ variables.js
@@ -0,0 +1,7 @@
+// var, let, const
+
+var names='Prats here';
+console.log(names);
+
+names = "Not prats";
+console.log(names);
