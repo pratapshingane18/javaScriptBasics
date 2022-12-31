581210711ef0e56b115e08f8a25b8c677c2ab263
pratapshingane18 <sushrutrao465@gmail.com>
2022-12-31 19:33:27 +0530

    Strings

--- TemplateLiterals.js
+++ TemplateLiterals.js
@@ -6,7 +6,25 @@ let html;
 
 //It is generally used when inserting HTML using JS, When Fetch Something Through AJAX
 
-// Without Template  STrings:
+// Without Template  Strings:
 html='<ul><li>Name: ' + names + '</li><li>Age: ' + age +'</li><li> ' + job + '</li><li>' + city+ '</li></ul>';
 
+
+//With Template Literals
+
+html = `
+<ul>
+<li>Name: ${names}</li>
+<li>Age: ${age}</li>
+<li>job: ${job}</li>
+<li>City: ${city}</li>
+
+
+
+</ul>
+
+    
+
+`
 document.body.innerHTML = html;
+
